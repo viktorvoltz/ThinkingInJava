bf54adef49212cd5e1387ef56b058abc01fc47d0
viktorvoltz <vctrchinyeaka@gmail.com>
2022-02-23 14:56:44 +0100

    mypackage

---
+++ AcessControl/Exercise1.java
@@ -0,0 +1,5 @@
+public class Exercise1 {
+    public static void main(String[] args){
+        mypackage.nonso.MypackageClass nonso = new mypackage.nonso.MypackageClass();
+    }
+}
---
+++ AcessControl/Exercise2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import mypackage.nonso.*;
+
+public class Exercise2 {
+    public static void main(String[] args){
+
+        mypackage.nonso.Vector vnonso = new mypackage.nonso.Vector();
+
+        Vector v = new Vector(); //collision here! Won`t compile!
+        java.util.Vector v1 = new java.util.Vector();
+    }
+}

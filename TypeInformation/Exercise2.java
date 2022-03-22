1bc09f9701ba7f313732779c362aac4fc261eab7
viktorvoltz <vctrchinyeaka@gmail.com>
2022-03-22 11:37:38 +0100

    TypeInformation 🏑

---
+++ TypeInformation/Exercise2.java
@@ -0,0 +1,73 @@
+/**
+ * @author viktorvoltz
+ */
+
+interface HasBatteries {
+}
+
+interface Waterproof {
+}
+
+interface Shoots {
+}
+
+interface Reverses{
+
+}
+
+class Toy {
+    // Comment out the following default constructor
+    // to see NoSuchMethodError from (*1*)
+    Toy() {
+    }
+
+    Toy(int i) {
+    }
+}
+
+class FancyToy extends Toy
+        implements HasBatteries, Waterproof, Shoots, Reverses {
+    FancyToy() {
+        super(1);
+    }
+}
+
+class ToyTest {
+    static void printInfo(Class<?> cc) {
+        System.out.println("Class name: " + cc.getName() +
+                " is interface? [" + cc.isInterface() + "]");
+                System.out.println("Simple name: " + cc.getSimpleName());
+                System.out.println("Canonical name : " + cc.getCanonicalName());
+    }
+
+    public static void main(String[] args) {
+        Class<?> c = null;
+        try {
+            c = Class.forName("FancyToy");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can’t find FancyToy");
+            System.exit(1);
+        }
+        printInfo(c);
+        for (Class<?> face : c.getInterfaces())
+            printInfo(face);
+        Class<?> up = c.getSuperclass();
+        Object obj = null;
+        try {
+            // Requires default constructor:
+            //cannot instantiate
+            obj = up.newInstance();
+            //System.out.println("obj " + obj + " " +obj.getClass().getName());
+        } catch (InstantiationException e) {
+            System.out.println("Cannot instantiate");
+            System.exit(1);
+        } catch (IllegalAccessException e) {
+            System.out.println("Cannot access");
+            System.exit(1);
+        } catch (NoSuchMethodError e){
+            System.out.println("NoSuchMethodError");
+            System.exit(1);
+        }
+        printInfo(obj.getClass());
+    }
+}

0d257ddeed1c207315e56069d345307678aa40c5
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-15 19:09:09 +0100

    singletonpatter

---
+++ mypackage/nonso/SingletonPattern.java
@@ -0,0 +1,31 @@
+/**
+ * @author viktorvoltz
+ */
+
+public class SingletonPattern {
+    private String _id;
+
+    private static SingletonPattern instance;
+
+    public static SingletonPattern getInstance(String id){
+        // check if instance has not been created
+        if(instance == null){
+            instance = new SingletonPattern(id);
+            return instance;
+        }else{
+            // if instance already exists
+            return instance;
+        }
+
+    }
+
+    //new object creation is inaccessible through constructor
+    private SingletonPattern(String id){
+        this._id = id;
+    }
+
+    @Override
+    public String toString() {
+        return _id;
+    }
+}

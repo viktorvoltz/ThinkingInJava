a218f4cc23ca74a85d15514fef445c8050c7af9f
viktorvoltz <vctrchinyeaka@gmail.com>
2022-06-14 11:47:05 +0100

    concurrency

---
+++ Concurrency/LiftOff.java
@@ -0,0 +1,32 @@
+public class LiftOff implements Runnable {
+    protected int countDown = 10; // Default
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff() {
+    }
+
+    public LiftOff(int countDown) {
+        this.countDown = countDown;
+    }
+
+    public String status() {
+        return "#" + id + "(" +
+                (countDown > 0 ? countDown : "Liftoff!") + "), ";
+    }
+
+    public void run() {
+        while (countDown-- > 0) {
+            System.out.print(status());
+            Thread.yield();
+        }
+    }
+}
+
+class BasicThreads {
+    public static void main(String[] args) {
+        Thread t = new Thread(new LiftOff());
+        t.start();
+        System.out.println("Waiting for LiftOff");
+    }
+}

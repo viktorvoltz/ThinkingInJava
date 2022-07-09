9e3d7183da243580ae82e5a227a2c8f8444d3f22
viktorvoltz <vctrchinyeaka@gmail.com>
2022-07-09 11:08:14 +0100

    singleThreading

---
+++ Concurrency/SingleThreading.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.*;
+
+public class SingleThreading {
+    public static void main(String[] args) { 
+        ExecutorService exec = 
+        Executors.newSingleThreadExecutor(); 
+        for(int i = 0; i < 5; i++) 
+        exec.execute(new LiftOff()); 
+        exec.shutdown(); 
+    }
+       
+}

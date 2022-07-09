d12ca13c7c185bd06132d6e681a51f9ecdbb47a2
viktorvoltz <vctrchinyeaka@gmail.com>
2022-07-09 11:07:44 +0100

    lift off

--- Concurrency/LiftOff.java
+++ Concurrency/LiftOff.java
@@ -15,6 +15,7 @@ public class LiftOff implements Runnable {
                 (countDown > 0 ? countDown : "Liftoff!") + "), ";
     }
 
+    @Override
     public void run() {
         while (countDown-- > 0) {
             System.out.print(status());
